cbdb7d4c6bb846b664f5f583b2b3b0855e6b0371
Narboux-Antoine <n42ant@gmail.com>
2017-03-08 13:57:29 +0100

    08/03/17 14h

--- test_index.php
+++ test_index.php
@@ -25,56 +25,93 @@ $player2= new Personnage("Dracofeu");
 //var_dump($player2);
 
 while ((!$player1->mort()) && (!$player2->mort())) {
-    $compteur++;
-    echo " "."<br>";
-    echo "ROUND ". $compteur." <br> ";
+
     ?>
-    <script>// var rep = prompt("Que voulez-vous faire ?");
-        //alert(rep);
-    </script>
-<?php
+    <label>
+        <input type="checkbox" id="cbox1" value="attaque">
+        Attaquer
+    </label>
+
+    <input type="checkbox" id="cbox2" value="potion"><label for="cbox2">
+        Utiliser Potion
+    </label>
+
+    <?php
+
     ////PREPARATION ATTAQUE ET DEFENSE////
     $player1->attaque($de->lanceDe());
     $player2->defense($de->lanceDe());
     $player2->attaque($de->lanceDe());
     $player1->defense($de->lanceDe());
 
+    if ($choix_attaque == "attaque") {
 
-    ////////////////////////////Phase attaque//////////////////////
-    echo "Phase Attaque"."<br>";
+        $compteur++;
+        echo " " . "<br>";
+        echo "ROUND " . $compteur . " <br> ";
+        ?>
+        <script>// var rep = prompt("Que voulez-vous faire ?");
+            //alert(rep);
+        </script>
+        <?php
+        ////////////////////////////Phase attaque//////////////////////
+        echo "Phase Attaque" . "<br>";
 
-     echo $player1->getNom()." a ".$player1->getAttaque()." en attaque<br>";
-     echo $player2->getNom(). " a ".$player2->getDefense(). " en défense<br>";
-    $player2->subit_attaque($player1->getAttaque(),$player2->getDefense());
+        echo $player1->getNom() . " a " . $player1->getAttaque() . " en attaque<br>";
+        echo $player2->getNom() . " a " . $player2->getDefense() . " en défense<br>";
+        $player2->subit_attaque($player1->getAttaque(), $player2->getDefense());
 
-    echo $player2->getNom(). " reçoit ".$player2->getDegat();
+        echo $player2->getNom() . " reçoit " . $player2->getDegat();
 
 
-    /////////Verification que l'adversaire soit toujours en vie//////
-    if($player2->mort()){
-        echo $player2->_nom." est K.O<br>";
-        break;
+        /////////Verification que l'adversaire soit toujours en vie//////
+        if ($player2->mort()) {
+            echo $player2->_nom . " est K.O<br>";
+            break;
+        }
+
+        echo $player1->getNom() . " a " . $player1->getSante() . " point de vie<br>";
+        echo $player2->getNom() . " a " . $player2->getSante() . " point de vie<br>";
+
+        ///////////////////////////Phase defense/////////////////////
+        echo "Phase Defense" . "<br>";
+
+
+        echo $player2->getNom() . " a " . $player2->getAttaque() . " en attaque<br>";
+        echo $player1->getNom() . " a " . $player1->getDefense() . " en défense<br>";
+        $player1->subit_attaque($player2->getAttaque(), $player1->getDefense());
+        echo $player1->getNom() . " reçoit " . $player1->getDegat();
+
+        /////////Verification que l'adversaire soit toujours en vie//////
+        if ($player1->mort()) {
+            echo $player1->_nom . " est K.O<br>";
+            break;
+        }
+        echo $player1->getNom() . " a " . $player1->getSante() . " point de vie<br>";
+        echo $player2->getNom() . " a " . $player2->getSante() . " point de vie<br>";
+    } else if ($choix_attaque == "potion") {
+
+        ///////Mettre en place potion//////
+
+
+        echo "Phase Defense" . "<br>";
+
+        echo $player2->getNom() . " a " . $player2->getAttaque() . " en attaque<br>";
+        echo $player1->getNom() . " a " . $player1->getDefense() . " en défense<br>";
+        $player1->subit_attaque($player2->getAttaque(), $player1->getDefense());
+        echo $player1->getNom() . " reçoit " . $player1->getDegat();
+
+        /////////Verification que l'adversaire soit toujours en vie//////
+        if ($player1->mort()) {
+            echo $player1->_nom . " est K.O<br>";
+            break;
+        }
+        echo $player1->getNom() . " a " . $player1->getSante() . " point de vie<br>";
+        echo $player2->getNom() . " a " . $player2->getSante() . " point de vie<br>";
     }
-
-    echo $player1->getNom(). " a ".$player1->getSante()." point de vie<br>";
-    echo $player2->getNom(). " a ".$player2->getSante()." point de vie<br>";
-
-    ///////////////////////////Phase defense/////////////////////
-    echo "Phase Defense"."<br>";
-
-
-    echo $player2->getNom()." a ".$player2->getAttaque()." en attaque<br>";
-    echo $player1->getNom(). " a ".$player1->getDefense(). " en défense<br>";
-    $player1->subit_attaque($player2->getAttaque(),$player1->getDefense());
-    echo $player1->getNom(). " reçoit ".$player1->getDegat();
-
-    /////////Verification que l'adversaire soit toujours en vie//////
-    if($player1->mort()){
-        echo $player1->_nom." est K.O<br>";
-        break;
+    else {
+        sleep(10);
     }
-    echo $player1->getNom(). " a ".$player1->getSante()." point de vie<br>";
-    echo $player2->getNom(). " a ".$player2->getSante()." point de vie<br>";
 }
 echo "Combat Terminé !!";
 
