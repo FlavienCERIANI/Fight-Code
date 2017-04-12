5c8933615890826f05c87ab9982873f22a7ef8a0
Narboux-Antoine <n42ant@gmail.com>
2017-04-12 09:43:47 +0200

    debut video2

--- test.php
+++ test.php
@@ -8,3 +8,4 @@ echo ' '
 echo '<video class="dragogg" width="100%" height="100%" controls="controls">';
  // echo '<source src='.$player1.'.ogg type="video/ogg" />';
  echo '<source src='.$player1->getNom().'_attaque_'.$player2->getNom().'.ogg type="video/ogg" />';
+ 
